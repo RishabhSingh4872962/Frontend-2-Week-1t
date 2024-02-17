8f14e1386a050d275c60e4a4eaca11a0d6e30c20
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-02-17 19:34:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((obj) => {
+    if (obj.marks > 50) {
+      console.log(obj);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((obj) => {
+    if (obj.marks > 50) {
+      console.log(obj);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({ id: 4, name: "susan", age: "20", marks: 45 });
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((obj) => obj.marks > 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  let arr2 = [
+    { rollNo: "100", name: "Akash" ,class:"12th" , gender:"male"},
+    { rollNo: "101", name: "Neil" ,class:"12th" ,gender :"male" },
+    { rollNo: "102", name: "Nitika", class:"11th" , gender:"female"},
+  ];
+  arr2.forEach((st)=>{
+    console.log(st);
+  })
 }
